a30bcebb709a6d72cf0f8e191a9430691c4dfd78
Alessaleit <alessandraleite1807@hotmail.com>
2025-06-10 15:14:06 -0300

    Primeiro commit: feat: adicionar módulo de produto

---
+++ test_produto.py
@@ -0,0 +1,11 @@
+from produto import (
+    cadastrar_produto
+)
+
+def test_cadastro_produto_v():
+    produto = cadastrar_produto("01", "Abacaxi", 11.11, 1)
+    assert isinstance(produto, dict)
+    
+def test_cadastro_produto_i():
+    resultado = cadastrar_produto("0", "X", 0, 0)
+    assert isinstance(resultado, str)
